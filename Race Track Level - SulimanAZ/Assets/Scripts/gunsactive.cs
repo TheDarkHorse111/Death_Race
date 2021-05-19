41074f98eaf14811db2a08166ef576f7010ea1ab
Anas Majdoub <km4jordan@gmail.com>
2021-05-19 14:38:44 +0300

    AddScripts

---
+++ Race Track Level - SulimanAZ/Assets/Scripts/gunsactive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunsactive : MonoBehaviour
+{
+     
+     
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Gunshooting.CanIFire = true;
+             Debug.Log("Disappear");
+             gameObject.SetActive(false);
+        }
+    
+}
---
+++ Race Track Level - SulimanAZ/Assets/Scripts/labcounter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class labcounter : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+         GameManager_Script.lab++; 
+      
+    }
+   
+}
