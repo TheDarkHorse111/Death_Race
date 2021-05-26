806123651296b25532ff9a3876cd22545048b5e3
TheDarkHorse111 <suliman.abuzaid98@gmail.com>
2021-05-26 17:03:30 +0300

    little work done

---
+++ Race Track Level - SulimanAZ/Assets/Scripts/RoadManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour
+{
+    public GameObject [] countdownItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject g in countdownItems)
+            g.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
